aeb3abf653e07a36cb15236e166a169ad314af52
Maurice004 <mauricealky@gmail.com>
2022-09-09 05:04:45 -0700

    printif

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 
-
-
 /**
-
- * main - Entry point
-
+ *main - Entry point
  *
-
- * Return: Always 0 (Success)
-
+ *Return: Always 0
  */
-
-
-
-int main(void)
-  
+int main (void)
 {
-  
-  printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-  
+  printf(" with proper grammar, but the outcome is a piece of art, \n");
+
   return (0);
-  
 }
