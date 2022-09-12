0a5779f181b8d84a62fbb39e9e279210db1a83a9
Maurice004 <mauricealky@gmail.com>
2022-09-12 03:20:27 -0700

    prints 0-9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-*main-prints the number from 0-9
+* main - Prints the number from 0-9.
 *return(0)
 (*
-*retuen:0 on success
+* Return: 0 on success
 */
 int main(void)
 {
-printf("0123456789\n");
+printf( "0123456789\n");
 return(0);
 }
